17e745a69ed11b68091904772f561815227a8fa4
Reshmi Ranjith <rreshmiranjith@gmail.com>
2021-09-03 15:47:51 -0500

    finished spring boot stuff

--- src/main/java/com/spring/basics/springin5steps/basic/BinarySearchImpl.java
+++ src/main/java/com/spring/basics/springin5steps/basic/BinarySearchImpl.java
@@ -1,14 +1,21 @@
 package com.spring.basics.springin5steps.basic;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
 @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class BinarySearchImpl {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     @Qualifier("quick")
@@ -19,6 +26,16 @@ public class BinarySearchImpl {
         System.out.println(sortAlgorithm);
         // search the array
         return 3;
+    }
+
+    @PostConstruct
+    public void postConstruct() { // called after bean is made
+        logger.info("postConstruct");
+    }
+
+    @PreDestroy
+    public void preDestroy() { // called right before the bean is removed from the context
+        logger.info("preDestroy");
     }
 
 }
